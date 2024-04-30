9a055cf8dea8a7bd5faadc1763ab34f163a94b5d
E Njang <104914526+Ekangaki@users.noreply.github.com>
2024-04-30 06:43:04 -0400

    Update EmpApp.py

--- aws-live-master/EmpApp.py
+++ aws-live-master/EmpApp.py
@@ -1,85 +1,52 @@
 from flask import Flask, render_template, request
-from pymysql import connections
-import os
+from pymysql import connect, Error
 import boto3
-from config import *
+from config import custombucket, customregion, customhost, customuser, custompass, customdb
 
 app = Flask(__name__)
-
-bucket = custombucket
-region = customregion
-
-db_conn = connections.Connection(
-    host=customhost,
-    port=3306,
-    user=customuser,
-    password=custompass,
-    db=customdb
-
-)
-output = {}
-table = 'employee'
-
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template('AddEmp.html')
 
-
 @app.route("/about", methods=['POST'])
 def about():
     return render_template('www.intellipaat.com')
 
-
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
-    emp_id = request.form['emp_id']
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    pri_skill = request.form['pri_skill']
-    location = request.form['location']
-    emp_image_file = request.files['emp_image_file']
-
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
-    cursor = db_conn.cursor()
-
-    if emp_image_file.filename == "":
-        return "Please select a file"
-
     try:
+        emp_id = request.form['emp_id']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        pri_skill = request.form['pri_skill']
+        location = request.form['location']
+        emp_image_file = request.files['emp_image_file']
 
+        # Connect to the MySQL database
+        conn = connect(host=customhost, user=customuser, password=custompass, database=customdb)
+        cursor = conn.cursor()
+
+        # Insert data into the database
+        insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
         cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
-        db_conn.commit()
-        emp_name = "" + first_name + " " + last_name
-        # Uplaod image file in S3 #
-        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-        s3 = boto3.resource('s3')
+        conn.commit()
 
-        try:
-            print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
+        # Upload image file to S3
+        s3 = boto3.client('s3')
+        s3_key = f"emp-id-{emp_id}_image_file"
+        s3.upload_fileobj(emp_image_file, custombucket, s3_key)
 
-            if s3_location is None:
-                s3_location = ''
-            else:
-                s3_location = '-' + s3_location
+        # Get the URL of the uploaded image
+        object_url = f"https://{custombucket}.s3.{customregion}.amazonaws.com/{s3_key}"
 
-            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                s3_location,
-                custombucket,
-                emp_image_file_name_in_s3)
-
-        except Exception as e:
-            return str(e)
-
-    finally:
         cursor.close()
+        conn.close()
 
-    print("all modification done...")
-    return render_template('AddEmpOutput.html', name=emp_name)
+        return render_template('AddEmpOutput.html', name=f"{first_name} {last_name}", image_url=object_url)
 
+    except Error as e:
+        return f"An error occurred: {e}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
