dbc5a4a0e17b579907437f2c28b8bbe740082231
E Njang <104914526+Ekangaki@users.noreply.github.com>
2024-05-09 14:55:13 -0400

    Update config.py

--- aws-live-master/config.py
+++ aws-live-master/config.py
@@ -1,7 +1,7 @@
 customhost = "employee.cphulrovocta.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "ekangaki1234"
-customdb = "employeedb"
+customdb = "employee"
 custombucket = "batch5-bucket"
 customregion = "us-east-1"
 
